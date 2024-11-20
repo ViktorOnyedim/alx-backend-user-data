f6f193baf5c383087b01094c9cf083f054e0a37f
ViktorOnyedim <viktoronyedim@gmail.com>
2024-11-20 16:08:01 +0100

    Modified script adhering to pycodestyle

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -7,40 +7,47 @@ from api.v1.views import app_views
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status() -> str:
-    """ GET /api/v1/status
-    Return:
-      - the status of the API
+    """
+    Handles GET requests to /api/v1/status.
+
+    Returns:
+        str: A JSON response indicating the status of the API.
     """
     return jsonify({"status": "OK"})
 
 
 @app_views.route('/stats/', strict_slashes=False)
 def stats() -> str:
-    """ GET /api/v1/stats
-    Return:
-      - the number of each objects
     """
+    Handles GET requests to /api/v1/stats.
 
+    Returns:
+        str: A JSON response containing the number of each objects
+    """
     from models.user import User
 
     stats = {}
     stats['users'] = User.count()
     return jsonify(stats)
 
+
 @app_views.route('/unauthorized', methods=["GET"], strict_slashes=False)
 def unauthorized() -> str:
     """
-    GET /api/v1/unauthorized
-    Return:
-      - 401 error using abort - Custom Error Pages
+    Handles GET requests to /api/v1/unauthorized.
+
+    Returns:
+        str: Triggers a 401 error using the abort function.
     """
     return abort(401)
+
 
 @app_views.route('/forbidden', methods=['GET'], strict_slashes=False)
 def forbidden() -> str:
     """
-    GET /api/v1/forbidden
-    Return:
-      - 403 error using abort - Custom Error Pages
+    Handles GET requests to /api/v1/forbidden
+
+    Returns:
+        str: Triggers a 403 forbidden error using the abort function
     """
     return abort(403)
