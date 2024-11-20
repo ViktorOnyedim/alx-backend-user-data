7175c4f2f83805f9eaa5b5602678f53b2a5da652
ViktorOnyedim <viktoronyedim@gmail.com>
2024-11-20 15:34:33 +0100

    Pycodestyle

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -6,13 +6,45 @@ from typing import List, TypeVar
 
 
 class Auth:
-    """Auth class"""
+    """
+    A class to handle authentication operations
+    """
+
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """
+        Determines if a given path requires authentication
+ 
+        Args:
+            path (str): The path to check
+            excluded_paths (List[str]): A list of paths that do not require authentication.
+
+        Returns:
+            bool: False
+        """
         return False
-    
+
+
     def authorization_header(self, request=None) -> str:
+        """
+        Retrieves the authorization header from a request.
+        Args:
+            request: The request object, optional.
+
+        Returns:
+            str: None
+        """
         return None
-    
+
+
     def current_user(self, request=None) -> TypeVar('User'):
+        """
+        Retrieves the current user from a request.
+        
+        Args:
+            request: The request object, optional.
+    
+        Returns:
+            TypeVar('User'): None
+        """
         return None
